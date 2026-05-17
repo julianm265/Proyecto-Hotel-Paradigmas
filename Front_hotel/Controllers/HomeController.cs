4f6ff121572366c140f018fd7b2091656c91d68c
Julian m <julian.munonozh@upb.edu.co>
2026-05-17 13:27:31 -0500

    Modelo MVC agregado

---
+++ Front_hotel/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Front_hotel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Front_hotel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
